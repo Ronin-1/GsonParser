33b776e173ab813e7794c0b1fecf2b8c8e423ad6
Kamil <kamil@Kamils-MacBook-Air.local>
2018-03-07 23:31:28 +0000

    Initial commit

---
+++ src/main/java/com/kamil/regex/Main.java
@@ -0,0 +1,12 @@
+package com.kamil.regex;
+
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        com.kamil.regex.Regex regex = new com.kamil.regex.Regex();
+        regex.replace();
+
+    }
+}
